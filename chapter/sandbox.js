d56c068fec938a5afdce3f3507621ecee9bb3ce8
100Niraj <nirajkumarpatel264@gmail.com>
2023-08-23 00:03:36 +0530

    update

--- chapter/sandbox.js
+++ chapter/sandbox.js
@@ -13,9 +13,7 @@
 
 // let radius = 10;
 // const pi = 3.14;
-// console.log(radius,pi);
-
-// //math operators +,-,*, / , ** , %
+// console.log(radius,pi);// //math operators +,-,*, / , ** , %
 // console.log(10/2);
 // let result = radius % 3;
 // console.log(result);
@@ -41,6 +39,7 @@
 // console.log(5 / 'hello');
 // console.log(5 * 'hello');
 
+// let likes = 10;
 // let res = 'the bolg has ' +  likes  + ' likes';
 // console.log(res);
 
@@ -104,10 +103,10 @@
 // console.log(ages[2]);
 
 // let random = ['niraj', 'luvly' , 21 , 19];
-// console.log(random);
+// console.log(random[1]);
 
 //length of array
-// console.log(niraj.length);
+// console.log(random.length);
 
 // //array methods
 // let results = niraj.join('-');
@@ -191,8 +190,8 @@
 // // let res = Number('hello');
 // // console.log(res); NaN
 
-// // let res = String(50);
-// // console.log(res , typeof res);
+//  let res = String(50);
+//  console.log(res , typeof res);
 
 // let res = Boolean(100);
 // console.log(res , typeof res);
@@ -215,7 +214,13 @@
 // if(age > 20){
 //     console.log('you are over 20 years old');
 // }
-
+// const age = 25;
+// if(age<20){
+//     console.log("your are 18+")
+// }
+// else {
+//     console.log('you are teener')
+//}
 // const password = 'p@ss';
 // if(password.length >= 8){
 //     console.log('that password is long enough !');
@@ -243,6 +248,11 @@
 
 // while(i<5){
 //     console.log('in loop:' , i);
+//     i++;
+// }
+// let i = 0;
+// while(i<5){
+//     console.log(i);
 //     i++;
 // }
 
@@ -570,6 +580,7 @@ console.log(para); */
 //     console.log(animals[i]);
 // }
 
+
 // Array.prototype.unshift()
 // the unshift() method andds one or more elements to the beginning of an array and returns the new length of the array;
 
@@ -588,9 +599,11 @@ console.log(para); */
 
 // const animals = ['pigs', 'cow', 'dogs', 'goats', 'sheep'];
 // const newanimals = animals.splice(3,0, "cats");
+// const newanimals2 = animals.splice(4,0, "monkey");
 // console.log(animals);
+// console.log(newanimals2);  //both give same output
 
-///output :-[ 'pigs', 'cow', 'dogs', 'cats', 'goats', 'sheep' ]
+///output :-[ 'pigs', 'cow', 'dogs', 'cats', 'monkey', 'goats', 'sheep' ]
 
 ///// UPDATE ELEMENTS //////
 
@@ -636,13 +649,18 @@ console.log(para); */
 // }
 // )
 // console.log(newArr);
+
 ////output :- [ false, false, false, false, true, true ]
-
-// let newArr = array1.map((currentElement, index, arr) => {
-//     return `Index no = ${index} and the value is ${currentElement} belong to ${arr}`
+// let newarr = array1.map((currentelement , index , arr) => {
+//     return currentelement < 7;
 // })
-// console.log(newArr);
+// console.log(newarr);
+//output :- [true,true,true,false,false,false]
 
+// let newarr = array1.map((currentelement, index , arr) => {
+//     return  `Index no = ${index} and the currentelement is ${currentelement} belong to ${arr}`
+// })
+// console.log(newarr);
 // output :- [
 //   'Index no = 0 and the value is 1 belong to 1,2,5,7,16,25',
 //   'Index no = 1 and the value is 2 belong to 1,2,5,7,16,25',
@@ -652,7 +670,7 @@ console.log(para); */
 //   'Index no = 5 and the value is 25 belong to 1,2,5,7,16,25'
 // ]
 
-// forEach((currentElement, index, arr)) == output (undefiend);
+//forEach((currentElement, index, arr)) == output (undefiend);
 
 // NOTE :- IT Return new array without mutating the orignal array
 // INTERVIEW Q  :- WHAT IS DIFF B\W MAP() AND forEach(); 
@@ -676,12 +694,14 @@ console.log(para); */
 
 // const array1 = [1, 2, 5, 7, 16, 25];
 
-// let sum = array1.reduce((accumulator,currentElement,index,arr) =>
-// {
-//     return accumulator += currentElement;
-// }//if we add sum extra int then we add here and put :- 8
-// )
-// console.log(sum);
+// let nwarr = array1.reduce((accumulator, currentelement,index ,arr) => {
+//     return accumulator += currentelement;
+// })
+// console.log(nwarr);
+// let nwarr2 = array1.reduce((accumulator, currentelement , index ,arr) => {
+//     return `previous element ${accumulator}, currentelement is ${currentelement}, Index is ${index}, and belong to ${arr}`
+// })
+// console.log(nwarr2);
 //output :- 56; , 64;
 
 
@@ -782,12 +802,17 @@ console.log(para); */
 //DOM in javascript
 //dom is basically DOCUMENT OBJECT MODEL 
 //WE USE FOR DOM की मदद से आप सभी html एलेमेंट्स (tags) को access और change कर सकते है · DOM की मदद से सभी html ... render to html page to excess;
-const myfunction = () =>
-            {
-                console.log(document.getElementById('demo').innerHTML ="this is my name");
+// const myfunction = () =>
+//             {
+//                 console.log(document.getElementById('demo').innerHTML ="this is my name");
 
-            }
+//             }
 // document.queryselector().innerHTML 
+// const myfunction = () => {
+//     console.log(document.getElementById('demo').innerHTML='this is my name');
+//     alert('hello niraj');
+// }
+// document.querySelector().innerHTML
 //use as same but जबकि querySelectorAll document में present सभी matched element की 
 //NodeList return करता है। इनके अलावा भी elements को select करने के तरीके है , 
 //आप किसी भी Element console में print कराकर आसानी से सभी functions बारे में जान सकते हैं। 
@@ -811,17 +836,242 @@ const myfunction = () =>
 
 
 
-// 4.way 
+// 4.way of writing events in js
 //1.using inline events alert();
 // 2.by calling a function (we already seem and most common way of writing)
 //3. using inline events (html onclick ="" property and element.onclick)  //overight kar deta hai 
 //4.using event listeners (addeventlistener and IE's attachevent);    fun ke ander fun call kar ke events ko karte hai     //multi time call  alert kar sakta hai uske baad console bhi kar deta hai so it is best;
 
 
-//// onchange()' :-
+// section 2: what is event object?
+// event object is the parent object of the event object.
+// for example mouseevent, focusevent, keyboardevent etc;
+
+//section 3. MouseEvent in javascript
+// the mouseevent object events that occurs when the mouse interacts with the document belongs to the MouseEvent Objects.
+// there are four event are types 
 
 
+//section 3 : keyboardEvent in javaScript
+// events that occur when user presse a key on the keyboard, belong to the keyboardEvent object. 
+//ther are there events from occurs by keyboardEvent 1.onkeypress="keypress()" 2.onkeydown="keydown()", 3.onkeyup="keyup()";
+          
 
+//section 4: InputEvents in JavaScipt
+//the Onchange event occurs when the value of an element has been changed;
+//for radiobuttons and checkboxes , the onchange event occurs when the 
+//checked state has been changed;
+
+// question is what is difference between onclick event and eventlistener 
+//ans:- this is because addeventListener does not overwrite existing event handlers, whereas onclick overrides any existing onclick = fn event handlers.
+//the other significant difference of course , is that onclick wil alwayas will alwayas work, where as addEventListner does not work in interenet Explorere before version 9;
+
+
+//////////************ Timing based Events ***************//////////
+//1.setTimeout()
+//2.setIntervel()
+//3.clearTimeout()
+//4.clearinterval()
+
+// The window object allows execution of code at specified time intervals
+
+// These time intervals are called timing events.
+
+// The tow key methods to use with JavaScript are:
+
+//setTimeout(function , millisecond) // Executes a function , after waiting a specified number of milliseconds.
+
+//setInterval(function, millisecond) // Same as setTimeout(), but repets the execution of the function continously
+
+// eg  <p id="showmyname"></p>
+// <br>
+// <button id="btn"> click here </button>
+// </br>
+
+// <script>
+
+//     const myname = document.querySelector("#showmyname");
+//     const butn = document.querySelector('#btn');
+
+//     const myname = () => {
+//         setTimeout( () => {
+//             myname.innerHTML = 'niraj kumar'
+//         }, 1000)
+//     }
+
+//     butn.addeventListener('click', myname);
+// </script>
+
+///              oops in javaScript          ///// 
+// 1. what is object literals\
+// 2.what is "this" objects
+
+
+// what is object literal 
+// ans:-  objecty literal is simply a key:value pair data structure;
+// sotring variable and funtion together in one container, we can refer this as an object.
+
+// how to create an object; 
+// 1. way 
+ 
+// let biodata = {
+//     myname: "niraj",
+//     myage: "22",
+//     getData : function () {
+//         console.log(`my name is${biodata.myname} and my age is ${biodata.myage}`);
+//     }
+// }
+// console.log(biodata);
+// console.log(biodata.myname);
+// console.log(biodata.getData());
+// //output
+// { myname: 'niraj', myage: '22', getData: [Function: getData] }
+// niraj
+// my name isniraj and my age is 22
+// undefined
+
+//2.way 
+// no need to write function as well after es6
+// ex . biodata.getData(); 
+
+// what is this object 
+
+// the definition of "this" object is that it contain the current context.
+
+// the this object can have different values depending on where it is placed .
+
+// for example 1
+// console.log(this);
+// it refers to the current context and that is window global object 
+
+// for example 2
+// function myName(){
+//     console.log(this);
+// }
+// myName();
+
+// for example 3
+// var myNames = "niraj";
+// function myname(){
+//     console.log(this.mynames);
+// }
+// myname();
+
+// for example 4;
+// const obj = {
+//     myage: 22,
+//     myName() {
+//         console.log(this.myage);
+//     }
+// }
+// obj.myName();
+
+// example 5;
+// const obj ={
+//     myage: 22,
+//     myName : () => {
+//         console.log(this);
+
+//     }
+// }
+// obj.myName(); oputcurrent obj not window obj and we not use fat arrow fun in ES2015;
+
+// what is output of this question 
+// let biodata = {
+//     myName: {
+//         realName : "niraj kumar",
+//         channelName : "thapa technical"
+//     },
+//     myage: 22,
+//     getData () {
+//         console.log(`my name is ${this.myName.realName} and where i study javascript channel name is ${this.myName.channelName}`);
+//     }
+// }
+// biodata.getData();
+// output :- my name is niraj kumar and where i study javascript channel name is thapa technical
+
+////////////////// destructuring in ES6 /////////////////////////////
+
+// the desturcturing assignment syntax is a javascript expression that makes it possible to unpack values from arrays 
+// or properties from objects, into distinct variables;
+
+ //// => Array Destructuring
+//const array = ['niraj', 'kumar', 22];   //basic array
+
+// let myFname = array[0];
+// let myLname = array[1];
+// let myAge = array[2];
+// console.log(myAge);
+
+// let [myfname,myFname,myAge] = array;
+// console.log(myAge);
+
+//// => object destructuring
+
+// const biodata ={
+//     myFname: 'niraj',
+//     myLname: 'kumar',
+//     age: 22
+// }
+// let age = biodata.age;
+// console.log(age);
+
+////////*********** Object properties ***************////////////
+
+// we can now use dynamic properties 
+// we can delcare any global variable then we can use variable in object through dynamic properties and do any mathematical operatin in this 
+// let myName = 'Niraj';
+// const biodata ={
+//     [myName]:"my name is this and",
+//     [20 + 2] : "my age"
+// }
+// console.log(biodata);
+// { '22': 'my age', Niraj: 'my name is this and' }
+
+/////////************** spread operator *********************/////////////
+
+// const colors = ['red','green','blue','white'];
+// const mycolors = ['red','green','blue','white','yellow','black'];
+
+// // 2nd time add one more color on top and tell we need to write it again on mycolor array to;
+
+// const myfavcolor = ['yello','black', ...colors];
+
+// console.log(myfavcolor);
+// // output ;-[ 'yello', 'black', 'red', 'green', 'blue', 'white' ] return copy of color array;
+
+
+///////////////************* Array Include ************* */
+// 1.array include
+// const colors = ['red','green','blue','white','pink'];
+// const isPresent = colors.includes('blue');
+// console.log(isPresent);  // output ;- true;
+
+// ES8
+// 1.string padding
+// 2.object.values();
+// 3.object.entries();
+
+
+// const array = {
+//     name: 'niraj',
+//     age: 22
+// }
+// console.log(Object.values(array)); 
+// console.log(Object.entries(array));
+// output
+// [ 'niraj', 22 ]
+// [ [ 'name', 'niraj' ], [ 'age', 22 ] ]
+
+// Es2019
+// Array.prototype.{Float32Array,flatmap}
+// Object.fromEntries();
+
+// Es2020
+// #1 BigInt
+// BigInt, one of the most anticipated features in JavaScript, is finally here. It actually allows developers to have much greater integer representation in their JS code for data processing for data handling.
+
+// At the moment the maximum number you can store as an integer in JavaScript is pow(2, 53) - 1 . But BigInt actually allows you to go even beyond that.  
 
 
 
